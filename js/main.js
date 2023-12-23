02496f26a600569d8c41f556edb777085511921e
Derevyanchenko2 <qxizerr10@gmail.com>
2023-12-23 22:41:11 +0200

    adding a project

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// burger-menu
+document.querySelector(".open-menu-js").addEventListener("click", function() {
+  var mobileMenuOverlay = document.querySelector(".mobileMenu-overlay");
+  mobileMenuOverlay.classList.add("open");
+  document.body.style.overflowX = "hidden";
+});
+
+document.querySelector(".mobileMenu-close").addEventListener("click", function() {
+  var mobileMenuOverlay = document.querySelector(".mobileMenu-overlay");
+  mobileMenuOverlay.classList.remove("open");
+  document.body.style.overflowX = "auto";
+});
+
