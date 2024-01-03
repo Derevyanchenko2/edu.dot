959553e55fb85d460ba8f830ca5a4f62b7f4504d
Derevyanchenko2 <qxizerr10@gmail.com>
2024-01-03 20:54:22 +0200

    Finished adaptive layout

--- js/main.js
+++ js/main.js
@@ -11,19 +11,12 @@ document.querySelector(".mobileMenu-close").addEventListener("click", function()
   document.body.style.overflowX = "auto";
 });
 
+
 function playVideo() {
-  // Получаем элементы
   const videoFrame = document.getElementById('videoFrame');
-  const processVideo = document.querySelector('.process-video');
-
-  // Скрываем фоновое изображение с плавным затухание
-
-  // Извлекаем URL видео
+  const processVideo = document.querySelector('.video');
   const videoUrl = 'https://www.youtube.com/embed/BNbPsiCGQzw?si=oCs2IsCeB1bp0cYh';
 
-  // Открываем новое окно или вкладку браузера с URL видео на YouTube
   window.open(videoUrl, '_blank');
-
-  // Отображаем iframe с видео
   videoFrame.style.display = 'block';
 }
